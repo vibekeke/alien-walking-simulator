8c8990b5c69a2d9f8160d1665c333191bbdd577e
vibekeke <107354551+vibekeke@users.noreply.github.com>
2024-09-29 14:38:36 +0200

    Initial Commit

---
+++ Player/PlayerOld.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const ACCELERATION = 800
+const MAX_SPEED = 100
+const FRICTION = 900
+
+
+var velocity = Vector2.ZERO
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) 
+	
+	set_velocity(velocity)
+	move_and_slide()
+	velocity = velocity
+	
